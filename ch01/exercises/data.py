4a2c68fe03ac53289564ba9782d6d4a7d57117b0
Your Name <you@example.com>
2023-01-30 17:22:17 -0500

    p

---
+++ ch01/exercises/data.py
@@ -0,0 +1,20 @@
+print(10*5)
+#should be 50
+print(10**2)
+#should be 100 (double ** means exponents)
+print(15/10) 
+#should be 1.5
+print(15//10)
+#should be 1 (integer division)
+print(-15//10)
+#should be -1 but rounds down I guess
+print (15%10)
+#should be 5 (division that returns the remainder)
+print (10%15)
+#I don't know what this should be
+print(10%10)
+#should be 0
+print (0%10)
+#should be 0
+print (10/15)
+#should be 0.66666666666
