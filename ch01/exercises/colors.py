debfd16916c1e5fedc34a66550ba1177a1e2bcd0
Your Name <you@example.com>
2023-02-01 16:53:25 -0500

    colors

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,10 @@
+colors = [
+    "red", "orange", "yellow", "green","blue", "violet"
+]
+print("I have ", len(colors), " colors to choose from.")
+print(colors[0])
+print(colors[1])
+print(colors[2])
+print(colors[3])
+print(colors[4])
+print(colors[5])
