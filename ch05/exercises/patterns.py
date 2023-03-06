528cf5ad6c47497b895d3a82d6b094a9ed1e970f
Your Name <you@example.com>
2023-03-06 18:06:58 -0500

    exercise

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,8 @@
+def star_pyramid():
+    star = "*"
+    rows = int(input("How many rows are in your pyramid?"))
+    for i in rows[rows]:
+        print (star)
+        star.append("*")
+
+star_pyramid()
