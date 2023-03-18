bcd9810059216dd9c5794262b07307e874e4910c
Your Name <you@example.com>
2023-03-17 22:50:07 -0400

    lab

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,5 +1,4 @@
 import pygame
-pygame.init()
 
 def threenp1(n):
     count = 0
@@ -15,25 +14,38 @@ def threenp1range(upper_limit):
     threenplus1_iters_dict = {}
     for n in range (2, upper_limit+1):
         threenplus1_iters_dict[n] = threenp1(n)
-    return threenplus1_iters_dict, threenplus1_iters_dict.items()
-    
+    return threenplus1_iters_dict
+
+def graph_coordinates(upper_limit):
+    threenplus1_iters_dict = threenp1range(upper_limit)
+    coor = threenplus1_iters_dict.items()
+
+    screen = pygame.display.set_mode()
+    screen.fill("mistyrose")
+    pygame.draw.lines(screen, "black", False, list(coor))
+    new_display = pygame.transform.flip(screen, False, True)
+    width, height = new_display.get_size()
+    new_display = pygame.transform.scale(screen, (width * 5, height * 5))
+    screen.blit(new_display, (0, 0))
+    max_so_far = (0,0)
+    for k,v in coor:
+        if v >= max_so_far[1]:
+            max_so_far = (k,v)
+        else:
+            max_so_far = max_so_far
+    font = pygame.font.Font(None, 30)
+    msg = font.render(str(max_so_far), False, "black", "gray")
+    screen.blit(msg, (10,10))
+    pygame.display.flip()
+    pygame.time.wait(4000)    
     
 def main():
+    pygame.init()
     upper_limit = int(input("Enter an upper limit:"))
     n = int(input("Enter a start number:"))
     print(threenp1range(upper_limit))
-    print(threenp1(n))
-
-def graph_coordinates(theenplus1_iters_dict):
-    screen = pygame.display.set_mode()
-    screen.fill("mistyrose")
-    screen_size = screen.get_size()
-    dimensions = [screen_size[0], screen_size[1]]
-    #coor = threenp1range()
-    #threenp1range()
-    #threenplus1_iters_dict[n] = threenp1(n)
-    pygame.draw.lines(screen, "black", True,)
-
+    graph_coordinates(upper_limit)
+ 
 main()
 
 
