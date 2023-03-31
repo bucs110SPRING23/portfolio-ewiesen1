c87bc8bc1cdfe14ece74f43331c97b41b69cb524
Your Name <you@example.com>
2023-03-31 11:39:16 -0400

    exercise

--- ch07/notes/main.py
+++ ch07/notes/main.py
@@ -1,15 +1,15 @@
-from point import ColorPoint
-import point, random, turtle, pygame
-from pygame import Rect
-
-
+"""
+file: main.py
+"""
+import random
+import point
+import pygame
 
 def mainloop(display):
-    import pygame
-    pygame.init()
-    display = pygame.display.set_mode()
     points = []
+    # mainloop
     while True:
+        # eventloop
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 point_deleted = False
@@ -18,52 +18,28 @@ def mainloop(display):
                         del points[i]
                         point_deleted = True
                 if not point_deleted:
-                    p = point.Point(event.pos[0], event.pos[1]) 
-                    #p = point.Point(*event.pos) #can use either one
+                    # t = turtle.Turtle()
+                    # p = point.Point(*event.pos)
+                    p = point.Point(event.pos[0], event.pos[1])
                     points.append(p)
+
+        # draw updates
         display.fill("white")
+
         for p in points:
-            pygame.draw.circle(display,p.color,p.rect.center, p.rect.h/2)
+            pygame.draw.circle(display, p.color, p.rect.center, p.rect.h / 2)
+
+        # show display
         pygame.display.flip()
 
 
 def main():
-    pygame.init
+
+    # initialize display and models
+    pygame.init()
     display = pygame.display.set_mode()
     mainloop(display)
+    pygame.quit()
+
 
 main()
-
-#skinability - how easy it is to change the visual interface
-
-#p1 = point.LED(x=100,y=100)
-
-#points = []
-#for p in range(10):
-#    x = random.randint(0,100)
-#    y = random.randint(0,100)
-#    points.append(point.Point("orange"))
-
-
-
-#pygame.draw.circle(display, p1.color, (p1.rect.x, p1.rect.y),p1.radius)
-
-#while True:
-#    pygame.display.flip()
-
-#p1.xcoor = 10 
-
-#print(p1.xcoor, p1.ycoor, p1.color, type(p1), id(p1))
-#print(p2.xcoor, p2.ycoor, p2.color, type(p2), id(p2))
-
-#points = []
-#for p in range(10):
- #   x = random.randint(0,100)
- #   y = random.randint(0,100)
- #   points.append(point.Point("orange"))
-
-#t = turtle.Turtle()
-#for p in points:
- #   p.random_color()
-  #  t.color(p.color)
-  #  t.goto(p.xcoor, p.ycoor)
--- ch07/notes/point.py
+++ ch07/notes/point.py
@@ -1,19 +1,16 @@
-import random, pygame
+"""
+File: point.py
+"""
+import random
+import pygame
 
-class ColorPoint:
-    """
-    docstring for Point
-    """
+class Point:
 
-    def __init__(self, x=0, y=0): 
-        #self.xcoor = abs(x)
-        #self.ycoor = abs(y)
+    def __init__(self, x=0, y=0, size=50):
+        # self ties the data to the objects scope rather than the function scope
         self.on = True
-        self.rect = pygame.Rect(abs(x), abs(y), 20, 20)
+        self.rect = pygame.Rect(abs(x), abs(y), size, size)
         self.color = self.random_color()
 
-
     def random_color(self):
-        return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-        
-    #no return
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
